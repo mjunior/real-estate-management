780642687bb6f29cab0e01d9de65e44706aa8e61
Mauricio <mauriciojs.junior@gmail.com>
2023-01-14 00:17:47 -0300

    feat: Dynamic capRate calculation

--- src/app/deals/deal-form/deal-form.component.ts
+++ src/app/deals/deal-form/deal-form.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { merge, Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-deal-form',
   templateUrl: './deal-form.component.html',
   styleUrls: ['./deal-form.component.scss']
 })
-export class DealFormComponent implements OnInit {
+export class DealFormComponent implements OnInit, OnDestroy {
   dealForm: FormGroup;
+  rateSubscription: Subscription;
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { 
     this.dealForm = this.fb.group({
       name: ['', Validators.required],
       purchasePrice: ['', Validators.required],
@@ -19,6 +19,28 @@ export class DealFormComponent implements OnInit {
       noi: ['', Validators.required],
       capRate: ['', Validators.required]
     });
+  }
+
+  ngOnInit() {
+    this.rateSubscription = this.capRateSubscription();
+  }
+
+  ngOnDestroy() {
+    this.rateSubscription.unsubscribe();
+  }
+
+  capRateSubscription() {
+    return merge(
+      this.dealForm.controls['noi'].valueChanges,
+      this.dealForm.controls['purchasePrice'].valueChanges
+    ).subscribe(() => {
+      const pPrice = this.dealForm.controls['purchasePrice'].value;
+      const noi = this.dealForm.controls['noi'].value;
+      
+      const capRate = noi/pPrice * 100;
+      
+      this.dealForm.patchValue({ capRate })
+    })
   }
 
 }
